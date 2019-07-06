f5f715ef05a39377b0f526d5bb876a5e4ea0db48
Boris Kornienko <kornienko.boris@hotmail.com>
2019-07-06 22:26:18 +0300

    Update parse_events_JSONs.go
    sql in json  parser

--- ParseJSONs/parse_events_JSONs.go
+++ ParseJSONs/parse_events_JSONs.go
@@ -10,7 +10,7 @@ import (
 	_ "github.com/denisenkom/go-mssqldb"
 	"database/sql"
 	"context"
-
+	"errors"
 )
 
 type ParsedEvents struct {
@@ -50,7 +50,7 @@ type ParsedEvents struct {
 		MachineName string `json:"machineName"`
 		DateNtime   string `json:"dateNtime"`
 		User        string `json:"user"`
-	} `json:"Syste_Critical"`
+	} `json:"System_Critical"`
 	Computer          string `json:"computer"`
 	ApplicationsError []struct {
 		Source      string `json:"Source"`
@@ -137,6 +137,53 @@ func selectProcessed(MachineFolder, fileDateName string) (int, error) {
 	return count, nil
 }
 
+func writeEvent(tableName string, eventID, eventSource, eventDescription, eventDateNtime, eventUser, string, eventCount int, eventFile ParsedEvents) (int64, error) {
+	if eventCount == 0{
+		return 0, nil
+	}
+	ctx := context.Background()
+    var err error
+
+    if db == nil {
+        err = errors.New("Write event: db is null")
+        return -1, err
+	}
+	
+	// Check if database is alive.
+	err = db.PingContext(ctx)
+	if err != nil {
+		return -1, err
+	}
+
+	tsql := "INSERT INTO WsEventsMonitor."+ tableName +" (machine, eventid, source, description, count, datentime, ip_v4, event_user) VALUES (@machine, @eventid, @source, @description, @count, @datentime, @ip_v4, @event_user);"
+	tsql = fmt.Sprintf(tsql)
+	stmt, err := db.Prepare(tsql)
+    if err != nil {
+       return -1, err
+	}
+	defer stmt.Close()
+
+	row := stmt.QueryRowContext(
+        ctx,
+        sql.Named("machine", eventFile.Computer),
+		sql.Named("eventid", eventID),
+		sql.Named("source", eventSource),
+		sql.Named("description", eventDescription),
+		sql.Named("count", eventCount),
+		// In this place we string, it's must be a MSSQL DateTime
+		// sql.Named("datentime", eventDateNtime),
+		// No :) IP must be writed by SQlfunction or with the Go string to binary convert
+		// sql.Named("ip_v4", eventFile.IP),
+		sql.Named("event_user", eventUser))
+    var newID int64
+    err = row.Scan(&newID)
+    if err != nil {
+        return -1, err
+    }
+
+	return newID, nil
+}
+
 func writeToDatabase(MachineFolder, DBUser, DBPassw, DBServerName, DBName string,  eventFile ParsedEvents) (int64, error) {
 	JSONFiles, err := ioutil.ReadDir(MachineFolder)
 	if err != nil {
@@ -162,6 +209,7 @@ func writeToDatabase(MachineFolder, DBUser, DBPassw, DBServerName, DBName string
 
 	
 	for _, f := range JSONFiles {
+		JSONPath := filepath.Join(MachineFolder, f.Name())
 		isProcessed, err := selectProcessed(MachineFolder, f.Name())
 		if err != nil {
 			log.Println(err)
@@ -169,16 +217,27 @@ func writeToDatabase(MachineFolder, DBUser, DBPassw, DBServerName, DBName string
 		}
 		if isProcessed != 0{
 			log.Println("this file is already: ", f.Name())
-			
+			err = os.Remove(JSONPath)
+			if err != nil {
+				log.Println("Cant delete ", JSONPath)
+			}
 			continue
 		}
-		JSONPath := filepath.Join(MachineFolder, f.Name())
+		
 		eventStruct, err := getToStruct(JSONPath)
 		if err != nil {
 			log.Fatal(err)
 			return -1, err
 		}
+		
+
+		// for _, systemCrit := range(eventStruct.SystemCritical){
+
+		// }
+		
 	}
+
+	// NO! Not is 1. It is temp!
 	return 1, err
 }
 
